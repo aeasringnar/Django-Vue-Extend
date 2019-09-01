73419e98eb12f3f4c33ea000448d0409c7cbf4a5
aeasringnar <aeasringnar@163.com>
2019-09-01 22:23:39 +0800

    更新测试多对多

--- backend/apps/flow/models.py
+++ backend/apps/flow/models.py
@@ -9,3 +9,13 @@ class FlowGroup(SoftDeleteModel, BaseModel):
         db_table = 'A_FlowGroup_Table'
         verbose_name = '审批组表'
         verbose_name_plural = verbose_name
+
+
+class FlowUser(SoftDeleteModel, BaseModel):
+    name = models.CharField(max_length=255, verbose_name='审批组内员工名称-test')
+    flow_group = models.ManyToManyField(Author, verbose_name='所属审批组', related_name='flow_users')
+
+    class Meta:
+        db_table = 'A_FlowUser_Table'
+        verbose_name = '审批组子表'
+        verbose_name_plural = verbose_name
