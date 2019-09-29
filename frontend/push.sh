1f2f11dab0d808c430dfb81316e1ab074f07e919
aeasringnar <aeasringnar@163.com>
2019-09-29 22:20:33 +0800

    update push.sh

--- frontend/push.sh
+++ frontend/push.sh
@@ -2,6 +2,5 @@
 proj_dir="extend"
 server="127.0.0.1"
 # npm run build
-ssh root@${server} "[[ ! -d /var/www/${proj_dir}/ ]] && mkdir -p  /var/www/${proj_dir}/"
-ssh root@${server} "[[  -d /var/www/${proj_dir}/ ]] && rm -rf /var/www/${proj_dir}/*"
+ssh root@${server} "rm -rf /var/www/${proj_dir}/*"
 scp -r dist/* root@${server}:/var/www/${proj_dir}/
