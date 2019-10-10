055fcc88945029f193781c58dd383c65779cd114
aeasringnar <aeasringnar@163.com>
2019-10-10 23:24:20 +0800

    关掉history模式

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -107,7 +107,7 @@ export const constantRouterMap = [
 ]
 
 export default new Router({
-  mode: 'history', //后端支持可开
+  // mode: 'history', //后端支持可开
   scrollBehavior: () => ({ y: 0 }),
   routes: constantRouterMap
 })
