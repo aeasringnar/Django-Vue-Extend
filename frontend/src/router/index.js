c422d3d2b941d89fb09aeea2b0b9904ad4e581f8
aeasringnar <aeasringnar@163.com>
2019-10-13 19:57:31 +0800

    增加用户可修改个人信息

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -108,6 +108,7 @@ export const constantRouterMap = [
 
 export default new Router({
   // mode: 'history', //后端支持可开
+  // base: '/abc/',
   scrollBehavior: () => ({ y: 0 }),
   routes: constantRouterMap
 })
